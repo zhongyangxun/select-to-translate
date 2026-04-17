046feabaf3f1032965357bd43c0fcf93ec8c4ae0
zhongyangxun <zhongyangxun@gmail.com>
2026-04-17 13:01:22 +0800

    chore: add a script to find the longest word with the most morphemes

---
+++ scripts/most-morpheme.js
@@ -0,0 +1,33 @@
+// 找出拥有最多词素单词中最长的一个，用来测试翻译面板的词根展示
+
+import fs from 'fs';
+
+const WORD_ROOTS_FILE = './data/word_roots.json';
+
+const wordRoots = JSON.parse(fs.readFileSync(WORD_ROOTS_FILE, 'utf8')).words;
+
+let res = null;
+
+Object.entries(wordRoots).reduce((maxCount, [word, roots]) => {
+  const count = roots.roots.length;
+
+  if (count >= maxCount) {
+    if (!res || word.length > res?.word?.length) {
+      res = {
+        word,
+        roots,
+      };
+    }
+
+    return count;
+  }
+
+  return maxCount;
+}, 0);
+
+const { word, roots } = res;
+
+console.log(
+  `拥有最多词素（${roots.roots.length}个）单词中最长的是：${word}，词素为：\n
+  ${roots.roots.map(({ root }) => root).join(', ')}`,
+);
